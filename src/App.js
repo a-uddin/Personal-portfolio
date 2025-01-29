76ca300726cccc7573ced85840b7a68b0a3006d0
Anowar <anowar.uddinaws@gmail.com>
2025-01-29 21:20:50 +0000

    Router fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 //import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Router, Routes, Route } from "react-router-dom"; 
+import { Routes, Route } from "react-router-dom"; 
 import { useNavigate } from "react-router-dom";
 
 import Header from "./components/Header";
@@ -119,7 +119,7 @@ function App() {
   };
 
   return (
-    <Router>
+    
       <div className="app-container">
         <Header />
         <div className="content-container">
@@ -412,7 +412,7 @@ function App() {
           />
         )}
       </div>
-    </Router>
+    
   );
 }
 
