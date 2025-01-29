018a1c074b493955cd332d81755aa1302ad58784
Anowar <anowar.uddinaws@gmail.com>
2025-01-29 20:23:05 +0000

    HashRouter added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 //import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import About from "./components/About";
