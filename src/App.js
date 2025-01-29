1c311a88e34f5eb683dac43f12d02cd90fc90326
Anowar <anowar.uddinaws@gmail.com>
2025-01-29 21:14:51 +0000

    Fixed Router import and added Babel plugin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 //import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom"; 
 import { useNavigate } from "react-router-dom";
 
 import Header from "./components/Header";
